1f1f23d35472d213c514fba03f986d02468351a7
KristianValkov <120708602+KristianValkov@users.noreply.github.com>
2023-11-24 18:02:48 +0200

    Add files via upload

---
+++ СтаниБогат123/Въпроси/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Въпроси
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblD = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblQ = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(42, 113);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(16, 16);
+            this.lblA.TabIndex = 1;
+            this.lblA.Text = "A";
+            this.lblA.Click += new System.EventHandler(this.lblA_Click);
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(42, 174);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(16, 16);
+            this.lblC.TabIndex = 2;
+            this.lblC.Text = "C";
+            this.lblC.Click += new System.EventHandler(this.lblC_Click);
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(183, 116);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(16, 16);
+            this.lblB.TabIndex = 3;
+            this.lblB.Text = "B";
+            this.lblB.Click += new System.EventHandler(this.lblB_Click);
+            // 
+            // lblD
+            // 
+            this.lblD.AutoSize = true;
+            this.lblD.Location = new System.Drawing.Point(182, 174);
+            this.lblD.Name = "lblD";
+            this.lblD.Size = new System.Drawing.Size(17, 16);
+            this.lblD.TabIndex = 4;
+            this.lblD.Text = "D";
+            this.lblD.Click += new System.EventHandler(this.lblD_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(205, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(64, 171);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(205, 113);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // lblQ
+            // 
+            this.lblQ.AutoSize = true;
+            this.lblQ.Location = new System.Drawing.Point(61, 56);
+            this.lblQ.Name = "lblQ";
+            this.lblQ.Size = new System.Drawing.Size(56, 16);
+            this.lblQ.TabIndex = 9;
+            this.lblQ.Text = "Въпрос";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 315);
+            this.Controls.Add(this.lblQ);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblD);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Стани по умен";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblD;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lblQ;
+    }
+}
+
---
+++ СтаниБогат123/Въпроси/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Въпроси
+{
+    public partial class Form1 : Form
+    {
+        ////1 step
+        //string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users" +
+        //    "\\STUDENTS-STEM-12\\AppData\\Local\\Quiz- 11a.mdf\";Integrated Security=True;Connect Timeout=30";
+        //SqlConnection connect = new SqlConnection(connectionString);
+        //connect.Open();
+        //MessageBox.Show("Добре е");
+        ////2 step
+        //string strinqQuery = "Select * from Questions";
+        //SqlCommand cmd = new SqlCommand(strinqQuery, sc);
+        ////3 step
+        //SqlDataReader reader = cmd.ExecuteReader();
+        ////4 step REad the reader
+        //while (reader.Read())
+        //{
+        //    lblA.Text = reader["A1"].ToString();
+        //}
+        //// step Closes
+        //reader.Close();
+        //connectionString.Close();
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users" +
+            "\\STUDENTS-STEM-12\\AppData\\Local\\Quiz- 11a.mdf\";Integrated Security=True;Connect Timeout=30";
+        SqlConnection c;
+        SqlCommand command;
+        SqlDataReader r;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            c = new SqlConnection(connectionString);
+            if(c.State==0)
+            {
+                c.Open();
+            }
+            string textQuery = "Select*from Qustions";
+            command = new SqlCommand(textQuery, c);
+            r=command.ExecuteReader();
+            LoadQ();
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void LoadQ()
+        {
+            if(r.Read())
+            {
+                r.Close();
+                c.Close();
+            }
+            r.Read();
+            lblQ.Text = r["Question"].ToString();
+            lblA.Text = r["Answer1"].ToString();
+            lblB.Text = r[3].ToString();
+            lblC.Text = r["Answer3"].ToString();
+            lblD.Text = r["Answer4"].ToString();
+        }
+        private void lblD_Click(object sender, EventArgs e)
+        {
+            if(string.Compare(lblD.Text, r["Correct"].ToString())==0)//lblD.Text == r["Correct"].ToString()
+            {
+                MessageBox.Show("Vqrno!");
+                lblD.BackColor = Color.Green;
+                lblD.ForeColor=Color.White;
+                ClearSccreen();
+                LoadQ();
+            }
+            else
+            {
+                lblD.BackColor = Color.Red;
+                lblD.ForeColor = Color.White;
+                DialogResult button=
+                MessageBox.Show("Greshno!","Begin?",MessageBoxButtons.YesNo);
+                if(button==DialogResult.Yes)
+                {
+                    r = command.ExecuteReader();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+        }
+        private void ClearSccreen()
+        {
+            lblA.BackColor= SystemColors.ControlText;
+            lblA.ForeColor = Color.DarkGreen;
+            lblB.BackColor = SystemColors.ControlText;
+            lblB.ForeColor = Color.DarkGreen;
+            lblC.BackColor = SystemColors.ControlText;
+            lblC.ForeColor = Color.DarkGreen;
+            lblD.BackColor = SystemColors.ControlText;
+            lblD.ForeColor = Color.DarkGreen;
+        }
+        private void lblB_Click(object sender, EventArgs e)
+        {
+            if (string.Compare(lblB.Text, r["Correct"].ToString()) == 0)//lblD.Text == r["Correct"].ToString()
+            {
+                MessageBox.Show("Vqrno!");
+                lblB.BackColor = Color.Green;
+                lblB.ForeColor = Color.White;
+                ClearSccreen();
+                LoadQ();
+            }
+            else
+            {
+                lblB.BackColor = Color.Red;
+                lblB.ForeColor = Color.White;
+                DialogResult button=
+                MessageBox.Show("Greshno!", "Begin?", MessageBoxButtons.YesNo);
+                if (button == DialogResult.Yes)
+                {
+                    r = command.ExecuteReader();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+        }
+        private void lblC_Click(object sender, EventArgs e)
+        {
+            if (string.Compare(lblC.Text, r["Correct"].ToString()) == 0)//lblD.Text == r["Correct"].ToString()
+            {
+                MessageBox.Show("Vqrno!");
+                lblC.BackColor = Color.Green;
+                lblC.ForeColor = Color.White;
+                ClearSccreen();
+                LoadQ();
+            }
+            else
+            {
+                lblC.BackColor = Color.Red;
+                lblC.ForeColor = Color.White;
+                DialogResult button =
+                MessageBox.Show("Greshno!", "Begin?", MessageBoxButtons.YesNo);
+                if (button == DialogResult.Yes)
+                {
+                    r = command.ExecuteReader();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+        }
+        private void lblA_Click(object sender, EventArgs e)
+        {
+            if (string.Compare(lblA.Text, r["Correct"].ToString()) == 0)//lblD.Text == r["Correct"].ToString()
+            {
+                MessageBox.Show("Vqrno!");
+                lblA.BackColor = Color.Green;
+                lblA.ForeColor = Color.White;
+                ClearSccreen();
+                LoadQ();
+            }
+            else
+            {
+                lblA.BackColor = Color.Red;
+                lblA.ForeColor = Color.White;
+                DialogResult button =
+                MessageBox.Show("Greshno!", "Begin?", MessageBoxButtons.YesNo);
+                if (button == DialogResult.Yes)
+                {
+                    r = command.ExecuteReader();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
+}
+;
